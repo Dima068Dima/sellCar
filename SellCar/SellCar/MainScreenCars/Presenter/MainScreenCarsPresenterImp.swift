f61781de7b9e700fd0e5867989cd7d926a15af06
Dima_68 <drdima68@yandex.ua>
2023-08-01 23:36:58 +0300

    Add MainScreenCarsPresenterImp

---
+++ SellCar/SellCar/MainScreenCars/Presenter/MainScreenCarsPresenterImp.swift
@@ -0,0 +1,13 @@
+//
+//  MainScreenCarsPresenterImp.swift
+//  SellCar
+//
+//  Created by Дмитрий Татаринцев on 01.08.2023.
+//
+
+import Foundation
+
+
+final class MainScreenCarsPresenterImp {
+    
+}
