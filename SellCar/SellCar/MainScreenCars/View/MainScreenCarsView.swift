a7c78c0882f40a8e3465c0c35919143111c25889
Dima_68 <drdima68@yandex.ua>
2023-08-01 23:17:33 +0300

    Add class MainScreenCarsView

---
+++ SellCar/SellCar/MainScreenCars/View/MainScreenCarsView.swift
@@ -0,0 +1,23 @@
+//
+//  MainScreenCarsView.swift
+//  SellCar
+//
+//  Created by Дмитрий Татаринцев on 01.08.2023.
+//
+
+import Foundation
+import SnapKit
+
+final class MainScreenCarsView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+     
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        assertionFailure("init(coder:) has not been implemented")
+        return nil
+    }
+}
