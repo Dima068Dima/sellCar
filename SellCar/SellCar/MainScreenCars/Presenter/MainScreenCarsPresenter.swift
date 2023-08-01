b429a9b13eabc2fff23f206f97a854c5528ce91f
Dima_68 <drdima68@yandex.ua>
2023-08-01 23:36:04 +0300

    Add MainScreenCarsViewController

---
+++ SellCar/SellCar/MainScreenCars/Presenter/MainScreenCarsPresenter.swift
@@ -0,0 +1,12 @@
+//
+//  MainScreenCarsPresenter.swift
+//  SellCar
+//
+//  Created by Дмитрий Татаринцев on 01.08.2023.
+//
+
+import Foundation
+
+protocol MainScreenCarsPresenter {
+    func cellDidTap(with eventId: String)
+}
---
+++ SellCar/SellCar/MainScreenCars/View/MainScreenCarsViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainScreenCarsViewController.swift
+//  SellCar
+//
+//  Created by Дмитрий Татаринцев on 01.08.2023.
+//
+
+import Foundation
+import UIKit
+
+final class MainScreenCarsViewController: UIViewController {
+    private let mainView = MainScreenCarsView()
+    private let presenter: MainScreenCarsPresenter
+    
+    init(presenter: MainScreenCarsPresenter) {
+        self.presenter = presenter
+        super.init(nibName: nil, bundle: nil)
+        setupActions()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        assertionFailure("init(coder:) has not been implemented")
+        return nil
+    }
+    
+    override func loadView() {
+        view = mainView
+    }
+    
+    private func setupActions() {
+       
+    }
+}
