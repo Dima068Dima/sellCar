6dd4a7a19685d8ea09b1ffaf8e13f5937a3d157c
Dima_68 <drdima68@yandex.ua>
2023-08-04 00:20:44 +0300

    Add model for posts

--- SellCar/SellCar/CarDetails/Presenter/CarDetailsPresenterImp.swift
+++ SellCar/SellCar/CarDetails/Presenter/CarDetailsPresenterImp.swift
@@ -8,50 +8,105 @@
 import Foundation
 
 final class CarDetailsPresenterImp: CarDetailsPresenter {
+    private weak var view: CarDetailsView?
+    private let carDetailsNetworkAPI = CarDetailsNetworkAPI()
+    private var carId: String
     
+    init(carId: String) {
+        self.carId = carId
+    }
+    
+    func loadDetailCar() {
+        Task {
+            do {
+                let carPostResponse = try await carDetailsNetworkAPI.getPostCar(with: carId)
+                await MainActor.run {
+                    view?.update(with: carPostResponse)
+                }
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    func setView(view: CarDetailsView) {
+        self.view = view
+    }
 }
 
 // MARK: - View Model -
 
 struct CarDetailsModel: Decodable {
+    var uuid = UUID()
+    
     let car: Car
     let user: OwnerDetails
 }
 
-struct Car: Decodable {
-    let uuid = UUID()
-    
+struct Car: Decodable, Hashable {
     let id: Int
     let name: String
     let uin: String
-  
+    
     enum CodingKeys: String, CodingKey {
-           case id
-           case name
-           case uin = "place_id"
-       }
+        case id
+        case name
+        case uin = "place_id"
+    }
 }
 
-struct OwnerDetails: Decodable {
-        let id: Int
-        let username, email, about: String
-        let avatar: Avatar
-        let autoCount: Int
-        let mainAutoName: String
-
-        enum CodingKeys: String, CodingKey {
-            case id, username, email, about, avatar
-            case autoCount = "auto_count"
-            case mainAutoName = "main_auto_name"
-        }
+struct OwnerDetails: Decodable, Hashable {
+    let id: Int
+    let username, email, about: String
+    let avatar: Avatar
+    let autoCount: Int
+    let mainAutoName: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, username, email, about, avatar
+        case autoCount = "auto_count"
+        case mainAutoName = "main_auto_name"
+    }
 }
 
-struct Avatar: Decodable {
+struct Avatar: Decodable, Hashable {
     let url: String
 }
 
-//extension Car: Hashable {
-//    static func == (lhs: Cars, rhs: Cars) -> Bool {
-//        lhs.uuid == rhs.uuid
-//    }
-//}
+extension CarDetailsModel: Hashable {
+    static func == (lhs: CarDetailsModel, rhs: CarDetailsModel) -> Bool {
+        lhs.uuid == rhs.uuid
+    }
+}
+
+// MARK: - View Model To Get Posts -
+
+struct Welcome: Codable {
+    let posts: [Post]
+}
+
+// MARK: - Post
+struct Post: Codable {
+    let uuid = UUID()
+    
+    let id: Int
+    let text: String
+    let likeCount: Int
+    let createdAt: String
+    let commentCount: Int
+    let img: String
+    
+    enum CodingKeys: String, CodingKey {
+        case id, text
+        case likeCount = "like_count"
+        case createdAt = "created_at"
+        case commentCount = "comment_count"
+        case img
+    }
+}
+
+extension Post: Hashable {
+    static func == (lhs: Post, rhs: Post) -> Bool {
+        lhs.uuid == rhs.uuid
+    }
+}
