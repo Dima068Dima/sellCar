a6d61ab19e02744af0f9988be95114c9a8255fc2
Dima_68 <drdima68@yandex.ua>
2023-08-04 00:19:22 +0300

    Add a method to get posts from the network

--- SellCar/SellCar/CarDetails/NetworkServices/CarDetailsNetworkAPI.swift
+++ SellCar/SellCar/CarDetails/NetworkServices/CarDetailsNetworkAPI.swift
@@ -9,20 +9,29 @@ import Foundation
 import Alamofire
 
 final class CarDetailsNetworkAPI {
-    private let header = HTTPHeader(
-        name: "Load Car Details",
-        value: "Bearer 787071117870711178707111307b63465677870787071111c6df96e084d083a36e52fc5")
-    
     func getReward(with carId: String) async throws -> CarDetailsModel {
         let url = "http://am111.05.testing.place/api/v1/car/\(carId)"
         
         return try await AF.request(
             url,
             method: .get,
-            headers: [header]
+            headers: [:]
         )
         .validate(statusCode: [200])
         .serializingDecodable(CarDetailsModel.self)
+        .value
+    }
+    
+    func getPostCar(with carId: String) async throws -> Welcome {
+        let url = "http://am111.05.testing.place/api/v1/car/\(carId)/posts"
+        
+        return try await AF.request(
+            url,
+            method: .get,
+            headers: [:]
+        )
+        .validate(statusCode: [200])
+        .serializingDecodable(Welcome.self)
         .value
     }
 }
